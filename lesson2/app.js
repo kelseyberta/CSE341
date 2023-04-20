4472c18687ccf9a303afdae4747fed04975164cf
kelseyberta <ber20002@byui.edu>
2023-04-20 16:43:10 -0400

    Team Acitivty L2

--- lesson2/app.js
+++ lesson2/app.js
@@ -1,23 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
-
+var express = require('express');
+var bodyParser = require('body-parser');
 const port = process.env.PORT || 8080;
-const app = express();
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
+const professionalRoutes = require('./routes/professional');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
-  })
-  .use('/', require('./routes'));
+  });
+  
 
-mongodb.initDb((err, mongodb) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
-  }
-});
+app.use('/professional', professionalRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on Port ${port}`);
+})
