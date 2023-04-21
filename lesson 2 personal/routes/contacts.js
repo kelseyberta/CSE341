cbb5a3fcf6326121b7b32435c332580e839831c4
Kelsey Berta <103976265+kelseyberta@users.noreply.github.com>
2023-04-20 21:39:18 -0400

    Delete lesson 2 personal directory

--- lesson 2 personal/routes/contacts.js
+++
@@ -1,9 +0,0 @@
-const routes = require('express').Router();
-
-const contactsController = require('../controllers/contacts')
-
-routes.get('/', contactsController.getAll);
-routes.get('/:id', contactsController.getSingle);
-
-
-module.exports = routes;
